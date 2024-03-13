907dacf5f91f8b01b95ac15cf22a8d3df55521ee
Philip Mueller <mail@philip.in-aachen.net>
2024-03-13 15:43:52 -0400

    normalize complexity

--- lib/attractor/calculators/ruby_calculator.rb
+++ lib/attractor/calculators/ruby_calculator.rb
@@ -13,7 +13,10 @@ module Attractor
       super do |change|
         flogger = Flog.new(all: true)
         flogger.flog(change[:file_path])
-        complexity = flogger.total_score
+        # lines of code, ignoring comments and empty lines
+        loc = File.readlines(change[:file_path])
+          .reject { |line| line.strip.empty? || line.strip.start_with?('#') }.size
+        complexity = flogger.total_score / loc
         details = flogger.totals
         [complexity, details]
       end
